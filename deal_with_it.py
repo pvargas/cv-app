dd7ba5cdb4c533d5f31ee1dd5c34627aef8487f7
Pablo Vargas <pablovargasjr@gmail.com>
2017-12-16 18:25:25 -0500

    better styling

--- deal_with_it.py
+++ deal_with_it.py
@@ -1,15 +1,32 @@
 import cv2
 
 
-eyes    = cv2.CascadeClassifier("haarcascades/haarcascade_eye.xml")
+eye     = cv2.CascadeClassifier("haarcascades/haarcascade_eye.xml")
 face    = cv2.CascadeClassifier("haarcascades/haarcascade_frontalface_alt.xml")
-image   = cv2.imread("glasses.png", -1) 
+image   = cv2.imread("glasses.png", -1)  # -1 includes the alpha channel
 capture = cv2.VideoCapture(0)
 
-def deal_with_it(eyes_cascade, face_cascade, glasses_img):
-    '''live meme generator'''
+def adjust_glasses(h, eyes_w, eyes_h):
+    ''' helper function that computes values to scale/translate glasses '''
 
-    #mask
+    # derive initial glasses possition/scale from eyes
+    v1 = eyes_h * (-1)
+    v2 = eyes_h * 5
+    u1 = eyes_w * (-1)
+    u2 = eyes_w * 8
+
+    # normalize values
+    u1 = max(0, u1) 
+    v1 = max(0, v1)
+    u2 = min(h, u2)    
+    v2 = min(h, v2)
+
+    return v1, v2, u1, u2
+
+def deal_with_it(eyes_cascade, face_cascade, glasses_img):
+    ''' function detects faces and places the classic meme sunglasses on the detected face(s)! '''
+
+    #creates mask
     glasses_mask     = glasses_img[:, :, 3]
     glasses_mask_inv = cv2.bitwise_not(glasses_mask)
 
@@ -18,74 +35,51 @@ def deal_with_it(eyes_cascade, face_cascade, glasses_img):
 
     while (True):
         # Capture video feed
-        unused, img = capture.read()
-        frame       = cv2.flip(img, 1)
+        img   = capture.read()[1]
+        frame = cv2.flip(img, 1)
     
         # grayscale
         gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        faces = face_cascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5,minSize=(30, 30),flags=cv2.CASCADE_SCALE_IMAGE)
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=2, minSize=(15, 25), flags=cv2.CASCADE_SCALE_IMAGE)
     
         for (x, y, w, h) in faces:
 
-            roi_gray  = gray[y:y+h, x:x+w]
-            roi_color = frame[y:y+h, x:x+w]
+            color = frame[y:y+h, x:x+w]
+            grayscale  = gray[y:y+h, x:x+w]
 
-            eyes = eyes_cascade.detectMultiScale(roi_gray)
+            eyes = eyes_cascade.detectMultiScale(grayscale)
     
-            for (nx,ny,nw,nh) in eyes: 
-                
-                glasses_width  =  nw * 4
-                glasses_height = orig_glasses_height * 2
-                area = glasses_width * glasses_height
-    
-                # possition
-                x1 = nx - glasses_width * 2
-                x2 = nx + nw + glasses_width * 2
-                y1 = ny + nh - glasses_height
-                y2 = ny + nh + glasses_height
-    
-                # black magic
-                if x1 < 0:
-                    x1 = 0
-                if x2 > w:
-                    x2 = w
-                if y1 < 0:
-                    y1 = 0                
-                if y2 > h:
-                    y2 = h
-    
-                # adjust bassed on black magic
-                glasses_height = y2 - y1
-                glasses_width  = x2 - x1
-                    
-                # rescale
-                glasses  = cv2.resize(glasses_img, (glasses_width,glasses_height), interpolation = cv2.INTER_AREA)
-                mask     = cv2.resize(glasses_mask, (glasses_width,glasses_height), interpolation = cv2.INTER_AREA)
-                mask_inv = cv2.resize(glasses_mask_inv, (glasses_width,glasses_height), interpolation = cv2.INTER_AREA)
+            for (eyes_x, eyes_y, eyes_w, eyes_h) in eyes:
+
+                v1, v2, u1, u2 = adjust_glasses(h, eyes_w, eyes_h)
+
+                # resize glasses and masks
+                glasses  = cv2.resize(glasses_img,      (u2 - u1, v2 - v1))
+                mask     = cv2.resize(glasses_mask,     (u2 - u1, v2 - v1))
+                mask_inv = cv2.resize(glasses_mask_inv, (u2 - u1, v2 - v1))
     
                 # some more black magic
-                roi = roi_color[y1:y2, x1:x2]
+                roi = color[v1:v2, u1:u2]
     
                 # difference between regions of interest
-                roi_bg = cv2.bitwise_and(roi,roi,mask = mask_inv)    
-                roi_fg = cv2.bitwise_and(glasses,glasses,mask = mask)
+                foreround  = cv2.bitwise_and(glasses, glasses, mask = mask)
+                background = cv2.bitwise_and(roi, roi, mask = mask_inv)
     
-                # combine regions
-                dst = cv2.add(roi_bg,roi_fg)               
-                roi_color[y1:y2, x1:x2] = dst
+                # combines fg and bg of the eyes/glasses region by adding them              
+                color[v1:v2, u1:u2] = cv2.add(foreround, background) 
 
                 break
     
-        # video feed
-        cv2.imshow("Deal With It @ SalemStateMemes.edu", frame)
+        # display video feed
+        cv2.imshow("Sunglass-o-matic", frame)
     
-        #exit
-        if cv2.waitKey(1) == 27: #esc key
+        # The Escape key has to be pressed to exit the video window
+        if cv2.waitKey(1) == 27:
             break  
     
     capture.release()
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-	deal_with_it(eyes, face, image)
+    deal_with_it(eye, face, image)
